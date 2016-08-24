5ee1848ebe03520c9a1b4da80b19e0a3fe3a8196
laniley <melanie.mende@me.com>
2016-08-24 10:41:14 +0200

    progress points

--- app/components/infection/skill-progress.js
+++ app/components/infection/skill-progress.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   infection: null,
   skill: null,
 
-  skill_progress: Ember.computed('infection.infection_skill_progresses.length', 'skill', function() {
+  skill_progress_points: Ember.computed('infection.infection_skill_progresses.length', 'skill', function() {
       var progresses = this.get('infection.infection_skill_progresses');
       var progress = 0;
       progresses.forEach(prog => {
@@ -17,11 +17,11 @@ export default Ember.Component.extend({
       return progress;
   }),
 
-  costs: Ember.computed('skill_progress', function() {
-    return this.get('skill_progress') + 1;
+  costs: Ember.computed('skill_progress_points', function() {
+    return this.get('skill_progress_points') + 1;
   }),
 
-  tooltip: Ember.computed('skill_progress', function() {
+  tooltip: Ember.computed('costs', function() {
     return `costs ${this.get('costs')} <img src=\"assets/images/helix_dark.png\" height=\"24px\" />`;
   })
 });
